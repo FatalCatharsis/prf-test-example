52bbbdc81f2dfeac9501ffa094681e654f60fc7f
Wyatt Kennedy <wykennedy@gmail.com>
2024-01-02 20:14:27 -0600

    Added self sign cert and basic prf test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-prf';
+
+  async testPrf() {
+    const firstSalt = new Uint8Array(new Array(32).fill(1)).buffer
+    var newCredential = await navigator.credentials.create({
+      publicKey: {
+          challenge: new Uint8Array([1, 2, 3, 4]),
+          rp: {
+              name : "test",
+              id: "localhost"
+          },
+          user: {
+              id: new Uint8Array([5, 6, 7, 8]),
+              name : "Wyatt",
+              displayName : "Wyatt"
+          },
+          pubKeyCredParams: [
+              {alg: -7, type : "public-key"},
+              {alg : -8, type : "public-key"},
+              {alg : -257, type : "public-key"}
+          ],
+          authenticatorSelection : {
+              userVerification: "required",
+              authenticatorAttachment :"cross-platform",
+          },
+          timeout : 60000,
+          attestation : "direct",
+          extensions :  {
+              prf: {
+                  eval: {
+                      first: firstSalt
+                  }
+              }
+          } as any
+      }
+    }) as any
+
+    console.log(newCredential!.getClientExtensionResults())
+    if (newCredential!.getClientExtensionResults().prf.enabled) {
+      alert("prf extension available")
+    } else {
+      alert("prf extension not available")
+    }
+  }
 }
